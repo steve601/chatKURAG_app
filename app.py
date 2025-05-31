cfb46dc5c921a7d5a31f314a5d42730051132475
Stephen Odhiambo <Steveotieno@users.noreply.huggingface.co>
2025-05-31 08:08:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from huggingface_hub import InferenceClient
 gr.Markdown("<center>Any Queries about Kenyatta University</center>")
 demo = gr.ChatInterface(
     fn = None,
-    chatbot = gr.Chatbot(placeholder = "Message me..."),
-    title = 'ChatKU',
-    description = 'ChatKU',
+    chatbot = gr.Chatbot(label = "ChatKU"),
+    title = 'Any Queries about Kenyatta University?',
     autoscroll = True,
     theme = 'soft-dark'
 )
