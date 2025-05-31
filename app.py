9f3a238b02b9ceb5e27048426bd600d0e7f1682a
Stephen Odhiambo <Steveotieno@users.noreply.huggingface.co>
2025-05-31 07:56:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ gr.Markdown("<center>Any Queries about Kenyatta University</center>")
 demo = gr.ChatInterface(
     fn = None,
     textbox = gr.Textbox(placeholder = "Message me..."),
-    container = False,
     title = 'ChatKU',
     autoscroll = True,
     theme = 'soft-dark'
